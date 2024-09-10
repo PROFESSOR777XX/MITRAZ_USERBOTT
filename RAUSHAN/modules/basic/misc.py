aa0b81b3c19f33e62416c3e29c32e71f5b8669dd
PROFESSOR <160086180+Fakecheater77x@users.noreply.github.com>
2024-09-10 09:38:22 +0530

    Update misc.py

--- RAUSHAN/modules/basic/misc.py
+++ RAUSHAN/modules/basic/misc.py
@@ -17,14 +17,14 @@ if ALIVE_TEXT:
    txt = ALIVE_TEXT
 else:
     txt = (
-        f"** ⚡️ 𝗔𝗟𝗣𝗛𝗔 ⚡️ **\n\n"
+        f"** ⚡️ 𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 ⚡️ **\n\n"
         f"❏ **𝐕ᴇʀsɪᴏɴ**: `2.1`\n"
         f"├• **𝐔ᴘᴛɪᴍᴇ**: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
         f"├• **𝐏ʏᴛʜᴏɴ**: `{python_version()}`\n"
         f"├• **𝐏ʏʀᴏɢʀᴀᴍ**: `{__version__}`\n"
-        f"├• **𝐒ᴜᴘᴘᴏʀᴛ-𝐂ʜᴀᴛ**: [|| ˹𝗔ʟᴘʜᴀ 𝗪ᴏʀʟᴅ˼ ||](t.me/WORLD_ALPHA)\n"
-        f"├• **𝐔ᴘᴅᴀᴛᴇs**: [𝗔𝗟𝗣𝗛𝗔](t.me/ALPHA_DPZ_WORLD)\n"
-        f"└• **𝐌ᴀsᴛᴇʀ**: [𝗥𝗔𝗨𝗦𝗛𝗔𝗡](t.me/ll_ALPHA_BABY_lll)"        
+        f"├• **𝐒ᴜᴘᴘᴏʀᴛ-𝐂ʜᴀᴛ**: [|| ˹𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ˼ ||](t.me/+qYRBJgZsARpkNWJl)\n"
+        f"├• **𝐔ᴘᴅᴀᴛᴇs**: [𝐔ᴘᴅᴀᴛᴇs](t.me/PROFESSOR_UPDATES)\n"
+        f"└• **𝐌ᴀsᴛᴇʀ**: [𝐏ʀᴏғᴇssᴏʀ](t.me/PROFESSOR_77X)"        
     )
 
 @Client.on_message(
@@ -57,7 +57,7 @@ async def repo(bot: Client, message: Message):
     await asyncio.sleep(1)
     await message.edit("Fetching Source Code.....")
     await asyncio.sleep(1)
-    await message.edit("Here is repo: \n\n\nhttps://github.com/TEAMPURVI/ALPHA_USERBOT\nFork & Give an ⭐")
+    await message.edit("Here is repo: \n\n\nhttps://github.com/Fakecheater77x/NXT_USERBOTT\nFork & Give an ⭐")
 
 
 @Client.on_message(filters.command("creator", ".") & filters.me)
