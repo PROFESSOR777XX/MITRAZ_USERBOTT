c0387cf36cde4e979e313eec08f9c0f68c8e018b
PROFESSOR777XX <professor7711x@gmail.com>
2024-09-11 07:16:23 +0530

    Update start.py

--- RAUSHAN/modules/bot/start.py
+++ RAUSHAN/modules/bot/start.py
@@ -9,17 +9,17 @@ from pyrogram import *
 from pyrogram.types import * 
 
 PHONE_NUMBER_TEXT = (
-    " ✦𝐇ᴇʏ..! 𝐌ᴀsᴛᴇʀ..!!👋!\n\n✦ 𝐈'ᴍ A 𝐏ᴏᴡᴇʀғᴜʟʟ 𝐏ʀᴏғᴇssᴏʀ's Iᴅ 𝐔sᴇʀʙᴏᴛ 𝐇ᴇʟᴘᴇʀ?\n\n‣ I Cᴀɴ Hᴇʟᴘ Yᴏᴜ Tᴏ Hᴏsᴛ Yᴏᴜʀ Lᴇғᴛ Cʟɪᴇɴᴛs. \n\n‣ Tʜɪs Is Sᴘᴇᴄɪᴀʟʟʏ Fᴏʀ Cʜᴀᴛ Fʏᴛᴇʀs\n\n‣ Nᴏᴡ /clone {send your PyroGram ᴠ2 String Session}"
+    " ✦𝐇ᴇʏ..! 𝐌ᴀsᴛᴇʀ..!!👋!\n\n✦ 𝐈'ᴍ A 𝐏ᴏᴡᴇʀғᴜʟ 𝐌ɪᴛᴢᴀ's Iᴅ 𝐔sᴇʀʙᴏᴛ 𝐇ᴇʟᴘᴇʀ?\n\n‣ I Cᴀɴ Hᴇʟᴘ Yᴏᴜ Tᴏ Hᴏsᴛ Yᴏᴜʀ Lᴇғᴛ Cʟɪᴇɴᴛs. \n\n‣ Tʜɪs Is Sᴘᴇᴄɪᴀʟʟʏ Fᴏʀ Cʜᴀᴛ Fʏᴛᴇʀs\n\n‣ Nᴏᴡ /clone {send your PyroGram ᴠ2 String Session}"
 )
 
 @app.on_message(filters.command("start"))
 async def hello(client: app, message):
     buttons = [
            [
-                InlineKeyboardButton("⚡Oᴡɴᴇʀ 💕⚡", url="t.me/PROFESSOR_77X"),
+                InlineKeyboardButton("⚡Oᴡɴᴇʀ 💕⚡", url="t.me/FuckTheSn1tches"),
             ],
             [
-                InlineKeyboardButton("⚡Cʜᴀɴɴᴇʟ 💕⚡", url="https://t.me/PROFESSOR_UPDATES"),
+                InlineKeyboardButton("⚡Cʜᴀɴɴᴇʟ 💕⚡", url="https://t.me/userbot_channel_tnw"),
             ],
             [
                 InlineKeyboardButton("⚡Sᴜᴘᴘᴏʀᴛ 💕⚡", url="https://t.me/+qYRBJgZsARpkNWJl"),
@@ -36,11 +36,11 @@ async def clone(bot: app, msg: Message):
     cmd = msg.command
     phone = msg.command[1]
     try:
-        await text.edit("ᴡᴀɪᴛ ғᴏʀ 2 ᴍɪɴᴜᴛᴇs ғɪʀ ᴘʀᴏғᴇssᴏʀ ᴘᴇʟᴛᴀ...💌")
+        await text.edit("𝐖𝐀𝐈𝐓 𝐅𝐎𝐑 𝐓𝐖𝐎 𝐌𝐈𝐍𝐔𝐓𝐄𝐒...💌")
                    # change this Directry according to ur repo
         client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="RAUSHAN/modules"))
         await client.start()
         user = await client.get_me()
-        await msg.reply(f" ᴊᴀ ᴘᴇʟ ᴅᴇ sᴀʙᴋᴏ ᴀʙ Pʀᴏғᴇssᴏʀ ᴜʀ ᴅᴀᴅᴅʏ🥵 {user.first_name} 💨.")
+        await msg.reply(f" 𝐓𝐍𝐖 𝐂𝐄𝐎 𝐌𝐈𝐓𝐙𝐀 𝐇𝐄𝐑𝐄 {user.first_name} 💨.")
     except Exception as e:
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
